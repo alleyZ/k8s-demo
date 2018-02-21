7b44624d7f0962f96a4ad35ed1372114b7b673be
alleyz <alleyz@126.com>
2018-02-21 08:17:34 +0800

    data-cube

--- deploy/skywalking-collector/image/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i "s/%zk_host%/${ZK_HOST}/" /usr/local/skywalking-collector/config/application.yml
-sed -i "s/%es_cluster_name%/${ES_CLUSTER_NAME}/" /usr/local/skywalking-collector/config/application.yml
-sed -i "s/%es_cluster_node%/${ES_CLUSTER_NODE}/" /usr/local/skywalking-collector/config/application.yml
-
-/usr/local/skywalking-collector/bin/collectorService.sh start
--- deploy/skywalking-web/image/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i "s/127\.0\.0\.1\:10800/${COLLECTOR_SVC}/" /usr/local/skywalking-web/config/collector_config.properties
-
-sed -i "s/debug/info/" /usr/local/skywalking-web/config/log4j2.xml
-
- /usr/local/skywalking-web/bin/web-service.sh start
